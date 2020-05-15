8346f68f2a70414e0df3fbfdbeb8631350eac955
Сергей <halygali@mail.ru>
2020-05-15 08:44:41 +0300

    Updates

---
+++ precache-manifest.3d2bb68be0dc428f61674c5dea3c5c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c64303490f1d52310b1367bc091ad58",
+    "url": "/todolist-ts-common/index.html"
+  },
+  {
+    "revision": "5b4ae3a5289c39c65084",
+    "url": "/todolist-ts-common/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "44995a9b26686ed3c4fc",
+    "url": "/todolist-ts-common/static/js/2.f109bd20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-ts-common/static/js/2.f109bd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4ae3a5289c39c65084",
+    "url": "/todolist-ts-common/static/js/main.b5025807.chunk.js"
+  },
+  {
+    "revision": "c4a0d9cdfe39385a4fe9",
+    "url": "/todolist-ts-common/static/js/runtime-main.25df50f3.js"
+  }
+]);
