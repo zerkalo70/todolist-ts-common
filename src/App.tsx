b81e589f7c1fbcd56fc22f3eb2e4964042d8e530
Сергей <halygali@mail.ru>
2020-05-15 16:17:23 +0300

    1,2,3 lessons after deploy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
     let [tasks, setTasks] = useState([
         {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "JavaScript", isDone: true},
         {id: v1(), title: "ReactJS", isDone: false},
         {id: v1(), title: "Rest API", isDone: false},
         {id: v1(), title: "GraphQL", isDone: false}
